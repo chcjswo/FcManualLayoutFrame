c58028dd51cbd8e35e34d808e93c230a0d5512e6
tony <chcjswo@gmail.com>
2017-03-29 20:09:45 +0900

    Initial commit :)

---
+++ FcManualLayoutFrame/FcManualLayoutFrame.h
@@ -0,0 +1,19 @@
+//
+//  FcManualLayoutFrame.h
+//  FcManualLayoutFrame
+//
+//  Created by Changsik Jeon on 2017. 3. 29..
+//  Copyright © 2017년 mocadev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FcManualLayoutFrame.
+FOUNDATION_EXPORT double FcManualLayoutFrameVersionNumber;
+
+//! Project version string for FcManualLayoutFrame.
+FOUNDATION_EXPORT const unsigned char FcManualLayoutFrameVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FcManualLayoutFrame/PublicHeader.h>
+
+
